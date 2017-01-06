e3b660a19b5d1ed72410c0e1eda76cceb43a4f7b
brunog <gont.bruno@gmail.com>
2017-01-06 18:37:47 +0100

    creation base du formulaire et rajout page recapitulatif

--- src/GB/LouvreBundle/Controller/LouvreController.php
+++ src/GB/LouvreBundle/Controller/LouvreController.php
@@ -4,6 +4,11 @@ namespace GB\LouvreBundle\Controller;
 use GB\LouvreBundle\Entity\Formulaire;
 use GB\LouvreBundle\Entity\Visiteur;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -17,7 +22,35 @@ class LouvreController extends Controller
     public function formulaireAction(Request $request)
     {
         $formulaire = new Formulaire();
-        $formulaire->setDuree(1);
+
+        $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $formulaire);
+        $formBuilder
+            ->add('calendrier', DateType::class)
+            ->add('duree', ChoiceType::class, array('choices' => array(
+                'Journee' => '1',
+                'Demi-journee' => '2'),
+                'multiple'=>false,'expanded'=>true))
+            ->add('mail', EmailType::class)
+            ->add('Valider', SubmitType::class);
+        $form = $formBuilder->getForm();
+
+        if($request->isMethod('POST'))
+        {
+            $form->handleRequest($request);
+
+            if($form->isValid())
+            {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($formulaire);
+                $em->flush();
+
+                $request->getSession()->getFlashBag()->add('notice', 'Visite bien enregistrée');
+                return $this->redirectToRoute('gb_louvre_recapitulatif', array('id' => $formulaire->getId()));
+            }
+        }
+
+        return $this->render('GBLouvreBundle:Louvre:formulaire.html.twig', array('form' => $form->createView()));
+        /*$formulaire->setDuree(1);
         $formulaire->setMail('gont@gmail.com');
 
         $visiteur1 = new Visiteur();
@@ -36,22 +69,38 @@ class LouvreController extends Controller
 
         $formulaire->addVisiteur($visiteur1);
         $formulaire->addVisiteur($visiteur2);
+*/
 
-
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($formulaire);
+        //$em = $this->getDoctrine()->getManager();
+       /* $em->persist($formulaire);
         $em->persist($visiteur1);
         $em->persist($visiteur2);
-        $em->flush();
+        $em->flush();*/
 
-        if($request->isMethod('POST'))
+      /*  if($request->isMethod('POST'))
         {
             $request->getSession()->getFlashBag()->add('notice', 'Visite bien enregistrée');
             return $this->redirectToRoute('gb_louvre_recapitulatif', array('id' => $formulaire->getId()));
         }
 
-        return $this->render('GBLouvreBundle:Louvre:formulaire.html.twig');
+        return $this->render('GBLouvreBundle:Louvre:formulaire.html.twig');*/
+    }
+
+    public function recapitulatifAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $formulaire = $em->getRepository('GBLouvreBundle:Formulaire')->find($id);
+
+        $listVisiteurs = $em->getRepository('GBLouvreBundle:Visiteur')->findBy(array('formulaire' => $formulaire));
+
+        return $this->render('GBLouvreBundle:Louvre:recapitulatif.html.twig', array(
+            'formulaires' => $formulaire,
+            'listVisiteur' => $listVisiteurs
+        ));
     }
 }
+
+
 
 
