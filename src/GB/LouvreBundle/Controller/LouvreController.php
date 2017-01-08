45891736545c596692df0f6cdc5501dfeb237e46
brunog <gont.bruno@gmail.com>
2017-01-08 14:29:09 +0100

    Début mise en place swiftmailer

--- src/GB/LouvreBundle/Controller/LouvreController.php
+++ src/GB/LouvreBundle/Controller/LouvreController.php
@@ -25,7 +25,41 @@ class LouvreController extends Controller
         $form->handleRequest($request);
         if ($request->isMethod('POST') && ($form->isValid()))
         {
+            /*--------------Recupere email----------------*/
+            $nom = $form->get('visiteurs')->getData();
 
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Hello Email')
+                ->setFrom('cerveza_974@hotmail.com')
+                ->setTo('gont.bruno@gmail.com')
+                ->setBody(
+                   /* $this->renderView(
+                    // app/Resources/views/Emails/registration.html.twig
+                        'Emails/registration.html.twig',
+                        array('name' => $name)
+                    ),
+                    'text/html'*/
+                   $nom
+                )
+                /*
+                 * If you also want to include a plaintext version of the message
+                ->addPart(
+                    $this->renderView(
+                        'Emails/registration.txt.twig',
+                        array('name' => $name)
+                    ),
+                    'text/plain'
+                )
+                */
+            ;
+            $this->get('mailer')->send($message);
+
+
+
+
+
+
+            //Récupére toutes les informations du formulaire visiteur.
             foreach ($form->get('visiteurs')->getData() as $visiteur)
             {
                 $formulaire->addVisiteur($visiteur);//lie
